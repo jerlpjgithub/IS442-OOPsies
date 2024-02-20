1c44b1c91697bcee5d077bbfb924f4f045fc157a
Asher <laiu.asher@gmail.com>
2024-02-21 00:04:02 +0800

    BookingDetails

---
+++ server/src/main/java/com/oopsies/server/entity/BookingDetails.java
@@ -0,0 +1,25 @@
+package com.oopsies.server.entity;
+
+import jakarta.persistence.*;
+import java.util.*;
+
+
+public class BookingDetails {
+    private String eventName;
+    private int numTickets;
+    private int paymentDetails;
+
+    public BookingDetails(String eventName, int numTickets, int paymentDetails) {
+        this.eventName = eventName;
+        this.numTickets = numTickets;
+        this.paymentDetails = paymentDetails;
+    }
+
+    public List<Object> getBookingDetails() {
+        List<Object> bookingDetails = new ArrayList<>();
+        bookingDetails.add(eventName);
+        bookingDetails.add(numTickets);
+        bookingDetails.add(paymentDetails);
+        return bookingDetails;
+    }
+}
