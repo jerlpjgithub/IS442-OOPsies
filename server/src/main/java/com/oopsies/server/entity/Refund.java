e859f7f1d290e84406c6834fd136a5d224d769e1
Herman Tan <hermanblueberry@gmail.com>
2024-02-28 12:16:28 +0800

    Update refund.java with getters and setters

--- server/src/main/java/com/oopsies/server/entity/Refund.java
+++ server/src/main/java/com/oopsies/server/entity/Refund.java
@@ -3,6 +3,7 @@ package com.oopsies.server.entity;
 
 import jakarta.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 
 @Entity
@@ -17,6 +18,7 @@ public class Refund {
   @JoinColumn(name = "booking_id", referencedColumnName = "bookingID")
   private Booking booking;
 
+  // Have to think how to map with payment. 
   // // OnetoOne with payment to get payment state related to booking 
   // @OneToOne
   // @Size(max = 50)
@@ -26,5 +28,47 @@ public class Refund {
   @Column(name = "refundDate")
   private Date refundDate;
 
+  /* Constructors */
+  public Refund() {
+  }
 
+  public Refund(int refundId, Booking booking, Date refundDate) {
+    this.refundId = refundId;
+    this.booking = booking;
+    this.refundDate = refundDate;
+  }
+
+  private boolean processPayment(){
+    /* 
+     * Updates Customer class accountBalance with this.amount. 
+     */
+
+    return false;
+  }
+
+  /* Getters and Setters */
+
+  public int getRefundId() {
+    return this.refundId;
+  }
+
+  public void setRefundId(int refundId) {
+    this.refundId = refundId;
+  }
+
+  public Booking getBooking() {
+    return this.booking;
+  }
+
+  public void setBooking(Booking booking) {
+    this.booking = booking;
+  }
+
+  public Date getRefundDate() {
+    return this.refundDate;
+  }
+
+  public void setRefundDate(Date refundDate) {
+    this.refundDate = refundDate;
+  }
 }
