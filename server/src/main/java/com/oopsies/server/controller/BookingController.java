14f9a10ff1521d749d6cf69cd58771bca0ac260d
Pixalesce <pixalesce@gmail.com>
2024-03-06 17:09:23 +0800

    update payment route

--- server/src/main/java/com/oopsies/server/controller/BookingController.java
+++ server/src/main/java/com/oopsies/server/controller/BookingController.java
@@ -9,6 +9,7 @@ import com.oopsies.server.services.BookingService;
 import com.oopsies.server.services.PaymentService;
 import com.oopsies.server.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,11 +26,10 @@ public class BookingController {
         Booking booking = new Booking();
     }
 
-    @RequestMapping(value = "/payment", method = RequestMethod.POST)
+    @RequestMapping(value = "/bookings/payment", method = RequestMethod.POST)
     // assuming that user ID is passed via request headers
-    public void createPayment(@RequestHeader Map<String, String> headers) {
+    public ResponseEntity<?> createPayment(@RequestParam("userId") String userId) {
         // TODO get user object
-//        String userId = headers.get("userId");
         // TODO get booking object
         // TODO process payment
 //        paymentService.processPayment();
