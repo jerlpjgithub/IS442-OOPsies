9297992cb801fe524029ed677841b3322f271b6d
Pixalesce <pixalesce@gmail.com>
2024-03-16 13:05:00 +0800

    feat: update booking controller, implemented events CR

--- server/src/main/java/com/oopsies/server/controller/EventController.java
+++ server/src/main/java/com/oopsies/server/controller/EventController.java
@@ -1,58 +1,60 @@
 package com.oopsies.server.controller;
 
+import java.util.*;
 import com.oopsies.server.dto.EventDTO;
-import com.oopsies.server.dto.PaymentDTO;
-import com.oopsies.server.entity.Payment;
-import com.oopsies.server.entity.User;
-import com.oopsies.server.exception.UserInsufficientFundsException;
+import com.oopsies.server.entity.Event;
 import com.oopsies.server.payload.response.MessageResponse;
 import com.oopsies.server.services.EventService;
-import com.oopsies.server.services.PaymentService;
-import com.oopsies.server.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
-public class PaymentController {
+public class EventController {
 
-    @Autowired
-    private PaymentService paymentService;
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
     @Autowired
     private EventService eventService;
 
-    public PaymentController(PaymentService paymentService){
-        this.paymentService = paymentService;
+    public EventController(EventService eventService){
+        this.eventService = eventService;
     }
 
-    @PostMapping(path = "/{userId}/create-payment")
-    public ResponseEntity<?> createPayment(@PathVariable(value="userId") long userId, @RequestParam int numTickets
-    ){
+    @PostMapping(path = "/create-event")
+    public ResponseEntity<?> createEvent(@RequestBody Event eventRequest){
         try {
-            Optional<User> user = userDetailsService.getUserById(userId);
-            if (user.isEmpty()) {
-                throw new IllegalArgumentException("User " + userId + " does not exist!");
-            }
-            paymentService.processPayment(user.get(),
-                    eventService.getEventById(1).get(),
-                    numTickets);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponse("Successfully created payment"));
-        } catch (UserInsufficientFundsException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            EventDTO eventDTO = eventService.createEvent(eventRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponse<>(
+                    201, "Event was created successfully!", eventDTO
+            ));
+        }
+        catch (IllegalArgumentException exc) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse<>(
+                    400, exc.getMessage(), null
+            ));
+        }
+        catch (Exception exc) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse<>(
+                    500, exc.getMessage(), null
+            ));
         }
     }
 
-    @GetMapping(path = "/get-payment/{user_id}")
-    public ResponseEntity<?> getPaymentsByUserId(@PathVariable(value="user_id") long userId){
-        List<PaymentDTO> payments = paymentService.getPaymentsByUserId(userId);
-        return ResponseEntity.status(HttpStatus.OK).body(payments);
+    @GetMapping(path = "/get-event/{event_id}")
+    public ResponseEntity<?> getEventsByUserId(@PathVariable("event_id") long event_id){
+        // try{
+        Optional<EventDTO> _events = eventService.getEventById(event_id);
+        return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse<>(
+                200, "successful", _events
+        ));
+    }
+
+    @GetMapping(path = "/get-event/all")
+    public ResponseEntity<?> getAllEvents(){
+        List<EventDTO> _events = eventService.getAllEvents();
+        return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse<>(
+                200, "successful", _events
+        ));
     }
 }
--- server/src/main/java/com/oopsies/server/payload/request/BookingRequest.java
+++ server/src/main/java/com/oopsies/server/payload/request/BookingRequest.java
@@ -1,2 +1,33 @@
-package com.oopsies.server.payload.request;public class BookingRequest {
+package com.oopsies.server.payload.request;
+
+import com.oopsies.server.entity.Booking;
+import jakarta.validation.constraints.NotBlank;
+
+public class BookingRequest {
+    @NotBlank
+    private long eventId;
+
+    @NotBlank
+    private int numTickets;
+
+    public BookingRequest(long eventId, int numTickets) {
+        this.eventId = eventId;
+        this.numTickets = numTickets;
+    }
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getNumTickets() {
+        return numTickets;
+    }
+
+    public void setNumTickets(int numTickets) {
+        this.numTickets = numTickets;
+    }
 }
