86866dc0916f925dd37e5b3cfe4d696b15024379
Felix <felixlidaili98@gmail.com>
2024-03-20 17:18:58 +0800

    feat: add booking navigation

--- client/src/components/navbar/components/Profile.js
+++ client/src/components/navbar/components/Profile.js
@@ -10,18 +10,29 @@ const Profile = () => {
   const authUser = JSON.parse(localStorage.getItem("authUser"));
 
   const handleMenuClick = async (e) => {
-    if (e.key === "1") {
-      await logout();
-      navigate('/login');
-    } else if (e.key === "0") {
-      navigate(`/profile/${authUser.id}`);
+    switch(e.key) {
+      case "0":
+        navigate(`/profile/${authUser.id}`);
+        break;
+      case "1":
+        navigate(`/booking/${authUser.id}`);
+        break;
+      case "2":
+        await logout();
+        navigate('/login');
+        break;
+      default:
+        console.log("Invalid option")
+        break;
     }
   };
 
+  /* NOTE: You will want to conditionally render different profile based on the user profile type */
   const profileMenu = (
     <Menu onClick={handleMenuClick}>
-      <Menu.Item key="0">Profile</Menu.Item>
-      <Menu.Item key="1">Logout</Menu.Item>
+      <Menu.Item key="0">My Profile</Menu.Item>
+      <Menu.Item key="1">My Bookings</Menu.Item>
+      <Menu.Item key="2">Logout</Menu.Item>
     </Menu>
   );
 
--- client/src/pages/customer/BookingPage.js
+++ client/src/pages/customer/BookingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BookingPage = () => {
   return (
-    <div>BookingPage</div>
+    <div>Booking History Page</div>
   )
 }
 
