9236d7cb9b68dff0e91c498284ac03785ef49b43
Felix <felixlidaili98@gmail.com>
2024-03-23 00:04:41 +0800

    feat: change to camelCase for payload

--- server/src/main/java/com/oopsies/server/dto/TicketDTO.java
+++ server/src/main/java/com/oopsies/server/dto/TicketDTO.java
@@ -5,67 +5,73 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Date;
 
 public class TicketDTO {
-    @JsonProperty("User first name")
-    private String first_name;
-    @JsonProperty("User last name")
-    private String last_name;
-    @JsonProperty("User email")
-    private String email;
-    @JsonProperty("Event venue")
-    private String event_venue;
-    @JsonProperty("Event date and time")
-    private Date event_dateTime;
-    @JsonProperty("Ticket ID")
-    private long ticket_id;
-    @JsonProperty("Booking ID")
-    private long booking_id;
-    @JsonProperty("Booking Date")
-    private Date booking_dateTime;
-    @JsonProperty("Redeemed")
-    private boolean redeemed;
-    @JsonProperty("isValid")
-    private boolean isValid;
+  @JsonProperty("userFirstMame")
+  private String first_name;
+  @JsonProperty("userLastName")
+  private String last_name;
+  @JsonProperty("userEmail")
+  private String email;
+  @JsonProperty("eventVenue")
+  private String event_venue;
+  @JsonProperty("eventDatetime")
+  private Date event_dateTime;
+  @JsonProperty("ticketId")
+  private long ticket_id;
+  @JsonProperty("ticketPrice")
+  private double ticket_price;
+  @JsonProperty("bookingID")
+  private long booking_id;
+  @JsonProperty("bookingDate")
+  private Date booking_dateTime;
+  @JsonProperty("redeemed")
+  private boolean redeemed;
+  @JsonProperty("isValid")
+  private boolean isValid;
 
+  public TicketDTO() {
+  }
 
-    public TicketDTO() { }
+  public void setFirst_name(String first_name) {
+    this.first_name = first_name;
+  }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
+  public void setLast_name(String last_name) {
+    this.last_name = last_name;
+  }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEvent_venue(String event_venue) {
+    this.event_venue = event_venue;
+  }
 
-    public void setEvent_venue(String event_venue) {
-        this.event_venue = event_venue;
-    }
+  public void setEvent_dateTime(Date event_dateTime) {
+    this.event_dateTime = event_dateTime;
+  }
 
-    public void setEvent_dateTime(Date event_dateTime) {
-        this.event_dateTime = event_dateTime;
-    }
+  public void setTicketPrice(double ticket_price) {
+    this.ticket_price = ticket_price;
+  }
 
-    public void setTicket_id(long ticket_id) {
-        this.ticket_id = ticket_id;
-    }
+  public void setTicket_id(long ticket_id) {
+    this.ticket_id = ticket_id;
+  }
 
-    public void setBooking_id(long booking_id) {
-        this.booking_id = booking_id;
-    }
+  public void setBooking_id(long booking_id) {
+    this.booking_id = booking_id;
+  }
 
-    public void setBooking_dateTime(Date booking_dateTime) {
-        this.booking_dateTime = booking_dateTime;
-    }
+  public void setBooking_dateTime(Date booking_dateTime) {
+    this.booking_dateTime = booking_dateTime;
+  }
 
-    public void setValid(boolean valid) {
-        this.isValid = valid;
-    }
+  public void setValid(boolean valid) {
+    this.isValid = valid;
+  }
 
-    public void setRedeemed(boolean redeemed) {
-        this.redeemed = redeemed;
-    }
+  public void setRedeemed(boolean redeemed) {
+    this.redeemed = redeemed;
+  }
 }
