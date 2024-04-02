d499aa4357f049cccd0b0f97b76ee21870d8f04a
jerlpjgithub <jerlpjgithub@gmail.com>
2024-04-02 16:21:37 +0800

    fix(securityfilter): Add logging for url requested

--- server/src/main/java/com/oopsies/server/config/jwt/AuthTokenFilter.java
+++ server/src/main/java/com/oopsies/server/config/jwt/AuthTokenFilter.java
@@ -32,6 +32,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        logger.info("Processing request to {}", request.getRequestURI());
         try {
             String jwt = parseJwt(request);
             if (jwt != null && !jwt.trim().isEmpty() && jwtUtils.validateJwtToken(jwt)) {
