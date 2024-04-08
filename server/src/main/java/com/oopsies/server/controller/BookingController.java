2a938e0c06607ae5be16b66e6ae3cd68a8f22e71
Pixalesce <pixalesce@gmail.com>
2024-04-08 14:54:25 +0800

    feat: add ticket redemption, onsite booking

--- server/src/main/java/com/oopsies/server/controller/BookingController.java
+++ server/src/main/java/com/oopsies/server/controller/BookingController.java
@@ -4,17 +4,17 @@ import java.util.*;
 import com.oopsies.server.dto.BookingDTO;
 import com.oopsies.server.dto.EventDTO;
 import com.oopsies.server.dto.TicketDTO;
-import com.oopsies.server.entity.Booking;
-import com.oopsies.server.entity.EmailStructure;
-import com.oopsies.server.entity.Event;
-import com.oopsies.server.entity.Ticket;
-import com.oopsies.server.entity.User;
+import com.oopsies.server.entity.*;
 import com.oopsies.server.exception.UserInsufficientFundsException;
 import com.oopsies.server.payload.request.BookingRequest;
+import com.oopsies.server.payload.request.OnsiteBookingRequest;
 import com.oopsies.server.payload.response.MessageResponse;
 import com.oopsies.server.repository.BookingRepository;
+import com.oopsies.server.repository.RoleRepository;
 import com.oopsies.server.repository.TicketRepository;
+import com.oopsies.server.repository.UserRepository;
 import com.oopsies.server.services.BookingService;
+import com.oopsies.server.services.EventService;
 import com.oopsies.server.services.TicketService;
 import com.oopsies.server.services.UserServiceImpl;
 
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -32,11 +33,23 @@ public class BookingController {
     @Autowired
     private BookingService bookingService;
 
+    @Autowired
+    private EventService eventService;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Autowired 
     private EmailController emailController;
 
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @Autowired
     private BookingRepository bookingRepository;
@@ -52,7 +65,7 @@ public class BookingController {
     }
 
     @PostMapping(path = "/create/{user_id}")
-    @PreAuthorize("hasAnyRole('ROLE_OFFICER') or #user_id == authentication.principal.id")
+    @PreAuthorize("hasAnyRole('ROLE_USER') and #user_id == authentication.principal.id")
     public ResponseEntity<?> createBooking(@PathVariable(value="user_id") long user_id, @RequestBody BookingRequest bookingRequest){
         try {
             long eventId = bookingRequest.getEventId();
@@ -64,7 +77,7 @@ public class BookingController {
             User user = userService.getUserById(user_id);
 
             //get booking parameters first
-            String name = user.getFirstName() + user.getLastName();
+            String name = user.getFirstName() + " " + user.getLastName();
             // String email = user.getEmail();
             String email = "laiu.asher@gmail.com"; //this will need to change when we have proper user emails
             long bookingID = bookingDTO.getBookingID();
@@ -109,6 +122,96 @@ public class BookingController {
         }
     }
 
+    @PostMapping(path = "/create/on-site")
+    @PreAuthorize("hasAnyRole('ROLE_OFFICER')")
+    public ResponseEntity<?> createOnsiteBooking(@RequestBody OnsiteBookingRequest bookingRequest){
+        try {
+            long eventId = bookingRequest.getEventId();
+            int numTickets = bookingRequest.getNumTickets();
+            String userEmail = bookingRequest.getUserEmail();
+
+            Optional<EventDTO> someEvent = eventService.getEventById(eventId);
+            if (someEvent.isEmpty()) {
+                throw new IllegalArgumentException("Event not found");
+            }
+            EventDTO event = someEvent.get();
+            double paymentAmount = event.getTicketPrice() * numTickets;
+
+            Optional<User> someUser = userRepository.findByEmail(userEmail);
+            User user;
+            if (someUser.isEmpty()) {
+                user = new User(
+                        userEmail,
+                        encoder.encode("newCustomer"),
+                        "New",
+                        "User",
+                        paymentAmount);
+
+                // Default Role User is set
+                Role userRole = roleRepository.findByName(EnumRole.ROLE_USER)
+                        .orElseThrow(() -> new RuntimeException(
+                                "Error: User role is not found."));
+                Set<Role> roles = new HashSet<>();
+                roles.add(userRole);
+
+                user.setRoles(roles);
+                System.out.println(user);
+                userRepository.save(user);
+            } else {
+                user = someUser.get();
+                user.incrementAccountBalance(paymentAmount);
+            }
+
+            BookingDTO bookingDTO = bookingService.createBooking(user.getId(), eventId, numTickets);
+
+            //call emailController method
+            //get user
+
+            //get booking parameters first
+            String name = user.getFirstName() + " " + user.getLastName();
+            String email = user.getEmail();
+//            String email = "laiu.asher@gmail.com"; //this will need to change when we have proper user emails
+            long bookingID = bookingDTO.getBookingID();
+            Date bookingDate = bookingDTO.getBookingDate();
+
+            //get event parameters
+            String eventName = event.getEventName();
+            Date eventDate = event.getDateTime();
+            Date refundDate = null;
+            String venue = event.getVenue();
+            String type = "Booking Confirmation";
+
+            //get ticket parameters
+            List<Ticket> tickets = ticketRepository.findTicketsByBooking(bookingRepository.findBookingById(bookingID));
+            List<Long> ticketIDs = new ArrayList<>();
+            for (Ticket ticket : tickets) {
+                ticketIDs.add(ticket.getId());
+            }
+            double ticketPrice = event.getTicketPrice();
+            double totalPrice = ticketPrice * tickets.size();
+
+            //send email
+            EmailStructure emailStructure = new EmailStructure(name, email, bookingID, bookingDate, eventName,
+                    eventDate, refundDate, venue, ticketIDs,
+                    totalPrice, 0, type);
+            emailController.sendEmail(email, emailStructure);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponse<>(
+                    201, "Booking was created successfully!", bookingDTO
+            ));
+        }
+        catch (IllegalArgumentException | UserInsufficientFundsException exc) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse<>(
+                    400, exc.getMessage(), null
+            ));
+        }
+        catch (Exception exc) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse<>(
+                    500, exc.getMessage(), null
+            ));
+        }
+    }
+
     @GetMapping(path = "/get/{user_id}")
     @PreAuthorize("hasAnyRole('ROLE_OFFICER') or #user_id == authentication.principal.id")
     public ResponseEntity<?> getBookingsByUserId(@PathVariable("user_id") long user_id){
@@ -128,7 +231,7 @@ public class BookingController {
             User user = booking.getUser();
 
             //get user details
-            String name = user.getFirstName() + user.getLastName();
+            String name = user.getFirstName() + " " + user.getLastName();
             // String email = user.getEmail();
             String email = "laiu.asher@gmail.com"; //this will need to change when we have proper user emails
             long bookingID = booking.getBookingID();
